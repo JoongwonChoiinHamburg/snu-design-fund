7050b4ba03ab5231a80148e57a9ecd9f673c0239
tc03011-dot <tc03011@snu.ac.kr>
2026-06-19 16:29:14 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,4 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 /* config options here */
 
-// X-Powered-By 헤더 제거
-poweredByHeader: false,
-};
-
 export default nextConfig;
