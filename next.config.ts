aded4967740ef7adc3865f1d8ea31b85741f4e4f
tc03011-dot <tc03011@snu.ac.kr>
2026-06-19 16:01:01 +0900

    Update next.config.ts
    서울대학교 취약점 점검 
    
    내용: HTTP 응답 헤더에 불필요한 서버 버전 정보가 노출되어 공격자가 해당 서버 버전에 적합한 CVE 취약점 공격 등을 시도해볼 수 있는 위험성이 존재합니다.
    
    방안: HTTP Server 응답 헤더 내 서버 버전 정보 상세 노출 제거(숨김처리)를 권고합니다.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+/* config options here */
+
+// X-Powered-By 헤더 제거
+poweredByHeader: false,
 };
 
 export default nextConfig;
